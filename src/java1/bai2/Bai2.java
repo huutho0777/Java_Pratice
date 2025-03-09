9b26a46cc107258b1b4bdc040435c4b4189e45f8
huutho6666 <huutho0322@gmail.com>
2025-03-09 15:51:45 +0700

    sua arraylist thanh linkedlist

--- src/java1/bai2/Bai2.java
+++ src/java1/bai2/Bai2.java
@@ -1,10 +1,10 @@
 package java1.bai2;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Bai2 {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
+        LinkedList<Integer> list = new LinkedList<>();
 
         list.add(1);
         list.add(2);
