39c40a948ad2fa746ccc15ad92df2457d0b37857
huutho6666 <huutho0322@gmail.com>
2025-03-09 15:42:00 +0700

    up bai 2

---
+++ src/java1/bai2/Bai2.java
@@ -0,0 +1,17 @@
+package java1.bai2;
+
+import java.util.ArrayList;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(5);
+        list.add(4);
+
+        System.out.println(list);
+
+    }
+}
