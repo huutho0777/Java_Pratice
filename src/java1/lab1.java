5ee188cd53f05ea84bf88db17e2b75add1075bce
huutho6666 <huutho0322@gmail.com>
2025-03-09 15:04:02 +0700

    Initial project

---
+++ src/java1/lab1.java
@@ -0,0 +1,10 @@
+package java1;
+
+import java.util.ArrayList;
+
+public class lab1 {
+    public static void main(String[] args) {
+        System.out.println("sdfmksndfkjs");
+        ArrayList arrayList = new ArrayList();
+    }
+}
