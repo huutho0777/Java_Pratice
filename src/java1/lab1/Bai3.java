8be3d8aca0fe098e95e7557a5db7b092afe2d062
huutho6666 <huutho0322@gmail.com>
2025-03-16 09:24:05 +0700

    Giai bai tap java lab1

---
+++ src/java1/lab1/Bai3.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Bai3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap chieu dai canh lap phuong");
+        int chieudai = scanner.nextInt();
+        double thetich = Math.pow(chieudai,3);
+        System.out.println("the tich hinh lap phuong la "+thetich);
+        scanner.close();
+    }
+}
