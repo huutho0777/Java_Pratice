72e39385be6f6cbea6961e9d464024817e72db97
huutho6666 <huutho0322@gmail.com>
2025-03-16 09:43:42 +0700

    up java 1 lab2

---
+++ src/java1/lab2/Bai1.java
@@ -0,0 +1,25 @@
+package java1.lab2;
+
+import java.util.Scanner;
+
+public class Bai1 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" nhap vao so a :");
+        float a = scanner.nextFloat();
+        System.out.println("nhap vao so b : ");
+        float b = scanner.nextFloat();
+        if(a==0){
+            if(b==0){
+                System.out.print("phuong trinh vo so nghiem ");
+            }else{
+                System.out.print("phuong trinh vo nghiem ");
+            }
+        }else{
+
+            float x= -b/a;
+            System.out.print(" phuong trinh co nghiem duy nhat "+x);
+        }
+        scanner.close();
+    }
+}
